9663c41043873385333907736bd7b934203d165c
Parkash <parkash.kumar@smsglobal.com>
2021-10-05 16:51:06 +1100

    Task - 2: Migrations of authors, comics and author_comics table.

---
+++ database/migrations/2021_10_05_054201_create_comics_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateComicsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comics', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title')->nullable();
+            $table->string('series_name')->nullable();
+            $table->string('description')->nullable();
+            $table->integer('page_count')->nullable();
+            $table->integer('thumbnail_url')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comics');
+    }
+}
